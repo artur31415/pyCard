af142bdaa0140a22d803216a0ad98d40916597a0
Artur Ferreira Moreira <artur31415926@gmail.com>
2022-02-15 12:00:48 -0300

    bug fixes

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ card_data = json.load(f)
 
 
 for a in range(5):
-    myCards.append(Card(current_dir + card_path, card_data['cards'][a], 4, (100 * a, 20)))
+    myCards.append(Card(current_dir + card_path, card_data['cards'][a], 4, (100 * a + card_pad_left, 20)))
 
 
 f.close()
@@ -50,6 +50,10 @@ selected_card = None
 
 print("rect > ", myCards[0].card_img.get_rect())
 
+def PointMinus(pointA, pointB):
+    return (pointA[0] - pointB[0], pointA[1] - pointB[1])
+
+######################################################################3
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -66,35 +70,37 @@ while running:
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_pos = pygame.mouse.get_pos()
             # get a list of all sprites that are under the mouse cursor
-            clicked_cards = [s for s in myCards if s.card_img.get_rect().collidepoint(mouse_pos)]
+            clicked_cards = [s for s in myCards if s.card_img.get_rect().collidepoint(PointMinus(mouse_pos, s.position))]
             if len(clicked_cards) > 0:
                 for x in range(len(clicked_cards)):
                     selected_card = clicked_cards[x]
-                    print(log_count, "# Card -> ", clicked_cards[x].json_data['name'], " clicked")
+                    print(log_count, "# Clicked Card -> ", clicked_cards[x].json_data['name'], " clicked")
                     log_count += 1
 
 
-        mouse_pos = pygame.mouse.get_pos()
-        if myCards[0].card_img.get_rect().collidepoint((mouse_pos[0] - myCards[0].position[0], mouse_pos[1] - myCards[0].position[1])):
-            print(log_count, "# mouse is over ", myCards[0].json_data['name'], "; card pos = ", myCards[0].position, "; mouse pos = ", pygame.mouse.get_pos())
-            log_count += 1
+        for myCard in myCards:
+            mouse_pos = pygame.mouse.get_pos()
+            colide_pos = PointMinus(mouse_pos, myCard.position) # (mouse_pos[0] - myCard.position[0], mouse_pos[1] - myCard.position[1])
+            if myCard.card_img.get_rect().collidepoint(colide_pos):
+                print(log_count, "# mouse is over ", myCard.json_data['name'], "; card pos = ", myCard.position, "; mouse pos = ", pygame.mouse.get_pos(), "; colide_pos = ", colide_pos)
+                log_count += 1
 
 
     # Fill the background with white
     screen.fill((255, 255, 255))
 
-
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
     for a in range(5):
-        screen.blit(myCards[a].card_img, (card_pad_left + myCards[a].position[0], myCards[a].position[1]))
+        screen.blit(myCards[a].card_img, (myCards[a].position[0], myCards[a].position[1]))
         screen.blit(newCard.card_img, (card_pad_left + 100 * a, 400))
 
+    
 
     if selected_card != None:
-        textsurface = myfont.render(selected_card.json_data['name'], False, (0, 0, 0))
-        screen.blit(textsurface, (selected_card.position[0], selected_card.position[1] + 30))
+        textsurface = myfont.render(selected_card.json_data['name'] + r"\nAtk " + str(selected_card.json_data['atk']) + r"\nDef " + str(selected_card.json_data['def']), False, (0, 0, 0))
+        screen.blit(textsurface, (selected_card.position[0], selected_card.position[1] + 70))
+    
+    #pygame.draw.circle(screen, (0, 0, 255), myCards[1].position, 10)
+    
     # Flip the display
     pygame.display.flip()
 
